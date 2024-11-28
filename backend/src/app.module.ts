534b1cfa40848aa461b9994fffa29db18d8b3c7d
Ahmed Essam Ahmed Hassan <ahmed.essam@student.giu-uni.de>
2024-11-28 13:31:53 +0200

    User Service and Controller Done!

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './database.module';  // Import the DatabaseModule
+import { DatabaseModule } from './database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
--- backend/src/schemas/user.schema.ts
+++ backend/src/schemas/user.schema.ts
@@ -5,20 +5,25 @@ export type UserDocument = User & Document;
 
 @Schema()
 export class User  {
-  @Prop({ required: true })
-  name: string;
+    @Prop({ required: true })
+    name: string;
 
-  @Prop({ required: true, unique: true })
-  email: string;
+    @Prop({ required: true, unique: true })
+    email: string;
 
-  @Prop({ required: true })
-  passwordHash: string;
+    @Prop({ required: true })
+    passwordHash: string;
 
-  @Prop({ required: true, enum: ['student', 'instructor', 'admin'] })
-  role: string;
+    @Prop({ required: true, unique: true })
+    dataOfBirth: Date;
 
-  @Prop({ required: false, default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png'})
-  profilePictureUrl: string;
+    @Prop({ required: true, enum: ['student', 'instructor', 'admin'] })
+    role: string;
+
+    @Prop({ required: false, default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png'})
+    profilePictureUrl: string;
+
+    readonly _id?: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
