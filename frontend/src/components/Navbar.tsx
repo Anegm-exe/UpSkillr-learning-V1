dde8d7749033328f29e801b08e17c1c20e8b8e23
yousefnegmeldin <yousefnegm@gmail.com>
2024-12-14 00:14:14 +0200

    changed image height in navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,17 +1,23 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import navbarcss from '../styles/navbar.module.css';
-import '../styles/globals.css';
+import Link from "next/link";
+import Image from "next/image";
+import navbarcss from "../styles/navbar.module.css";
+import "../styles/globals.css";
 
 const Navbar = () => {
-    return (
-        <nav className={navbarcss.navbar}>
-            <Image src="/images/Logo_Simple.png" alt="Logo" className={navbarcss.img} width={800} height={250}></Image>
-            <Link href="/" className={navbarcss.button}>Home</Link>
-            <Link href="/about" className={navbarcss.button}>About</Link>
-            <Link href="/login" className={navbarcss.button}>Login</Link>
-        </nav>
-    );
+  return (
+    <nav className={navbarcss.navbar}>
+      <Image src="/images/Logo_Simple.png" alt="Logo" className={navbarcss.img} width={800} height={65}></Image>
+      <Link href="/" className={navbarcss.button}>
+        Home
+      </Link>
+      <Link href="/about" className={navbarcss.button}>
+        About
+      </Link>
+      <Link href="/login" className={navbarcss.button}>
+        Login
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
