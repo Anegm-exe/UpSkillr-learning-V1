96b9066ea8c3d67fb36db29aa282846aceaa6594
yousefnegmeldin <yousefnegm@gmail.com>
2024-12-16 22:53:01 +0200

    latest changes to backend and frontend

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,54 +1,53 @@
-import { Controller, Get, Delete, Body, Param, UseGuards, Req, Patch } from '@nestjs/common';
-import { UserService } from '../user/user.service';
-import { User } from './model/user.schema';
-import { Role, Roles } from 'src/Auth/decorators/roles.decorator';
-import { authorizationGuard } from 'src/Auth/guards/authorization.guard';
-import { updateUserDto, UserDto } from './dtos/user.dto';
-import { Request } from 'express';
+import { Controller, Get, Delete, Body, Param, UseGuards, Req, Patch } from "@nestjs/common";
+import { UserService } from "../user/user.service";
+import { User } from "./model/user.schema";
+import { Role, Roles } from "src/Auth/decorators/roles.decorator";
+import { authorizationGuard } from "src/Auth/guards/authorization.guard";
+import { updateUserDto, UserDto } from "./dtos/user.dto";
+import { Request } from "express";
 
-@Controller('user')
+@Controller("user")
 export class UserController {
-    constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
 
-    @Roles(Role.Admin)
-    @UseGuards(authorizationGuard)
-    @Get()
-    async findAll(): Promise<UserDto[]> {
-        return this.userService.findAll();
-    }
+  @Roles(Role.Admin)
+  @UseGuards(authorizationGuard)
+  @Get()
+  async findAll(): Promise<UserDto[]> {
+    return this.userService.findAll();
+  }
+  @Get("/me")
+  @UseGuards(authorizationGuard)
+  async getMyDetails(@Req() request: Request) {
+    return await this.userService.getMyDetails(request);
+  }
 
-    @Get(':id')
-    async findOne(@Param('id') id: string, @Req() req: Request): Promise<User> {
-        return this.userService.findOne(id,req);
-    }
+  @Get("students/course/:courseId")
+  async findStudentsByCourse(@Param("courseId") courseId: string): Promise<UserDto[]> {
+    return await this.userService.getStudentsInCourse(courseId);
+  }
 
-    @Patch(':id')
-    async update(
-        @Param('id') id: string,
-        @Body() updateUserDto: updateUserDto,
-        @Req() req: Request
-    ): Promise<User> {
-        return this.userService.update(id, updateUserDto, req);
-    }
+  @Get("instructor/course/:courseId")
+  async findInstructorByCourse(@Param("courseId") courseId: string): Promise<UserDto[]> {
+    return await this.userService.getInstructorsInCourse(courseId);
+  }
 
-    @Delete(':id')
-    async delete(@Param('id') id: string, @Req() req: Request): Promise<void> {
-        return this.userService.delete(id,req);
-    }
+  @Get("search/:name")
+  async searchByName(@Param("name") name: string): Promise<UserDto[]> {
+    return await this.userService.searchByName(name);
+  }
+  @Get(":id")
+  async findOne(@Param("id") id: string, @Req() req: Request): Promise<User> {
+    return this.userService.findOne(id, req);
+  }
 
-    @Get('students/course/:courseId')
-    async findStudentsByCourse(@Param('courseId') courseId: string): Promise<UserDto[]> {
-        return await this.userService.getStudentsInCourse(courseId);
-    }
+  @Patch(":id")
+  async update(@Param("id") id: string, @Body() updateUserDto: updateUserDto, @Req() req: Request): Promise<User> {
+    return this.userService.update(id, updateUserDto, req);
+  }
 
-    @Get('instructor/course/:courseId')
-    async findInstructorByCourse(@Param('courseId') courseId: string): Promise<UserDto[]> {
-        return await this.userService.getInstructorsInCourse(courseId)
-    }
-
-    @Get('search/:name')
-    async searchByName(@Param('name') name: string): Promise<UserDto[]> {
-        return await this.userService.searchByName(name)
-    }
-
+  @Delete(":id")
+  async delete(@Param("id") id: string, @Req() req: Request): Promise<void> {
+    return this.userService.delete(id, req);
+  }
 }
