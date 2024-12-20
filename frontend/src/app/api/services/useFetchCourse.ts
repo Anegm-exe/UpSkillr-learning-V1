c78cb5e6cf9c42d07685ad57ddcb50118b700865
Ahmed Essam Ahmed Hassan <ahmed.essam@student.giu-uni.de>
2024-12-20 21:22:24 +0200

    dashboard added :D

--- frontend/src/app/api/services/useFetchCourse.ts
+++ frontend/src/app/api/services/useFetchCourse.ts
@@ -1,27 +1,53 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 
 import { useState, useEffect } from 'react';
 import axios from '../../api/axios';
 
 export function useFetchCourse(courseId: string) {
-  const [courseData, setCourseData] = useState<any>(null);
-  const [error, setError] = useState<string | null>(null);
+    const [courseData, setCourseData] = useState<any>(null);
+    const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
+    useEffect(() => {
     if (courseId) {
-      const fetchCourseData = async () => {
+        const fetchCourseData = async () => {
         try {
-          const response = await axios.get(`/course/${courseId}`);
-          setCourseData(response.data);
+            const response = await axios.get(`/course/${courseId}`);
+            setCourseData(response.data);
         } catch (error) {
-          console.error('Error fetching course data:', error);
-          setError('Failed to fetch course data.');
+            console.error('Error fetching course data:', error);
+            setError('Failed to fetch course data.');
         }
-      };
+        };
 
-      fetchCourseData();
+        fetchCourseData();
     }
-  }, [courseId]);
+    }, [courseId]);
 
-  return { courseData, error };
+    return { courseData, error };
+}
+
+export function useFetchUserCourses() {
+    const [enrolledCourseData, setEnrolledCourseData] = useState<any>([]);
+    const [completedCourseData, setCompletedCourseData] = useState<any>([]);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchCourseData = async () => {
+            try {
+                const response1 = await axios.get(`/course/completed`);
+                const response2 = await axios.get(`/course/enrolled`);
+                setEnrolledCourseData(response2.data);
+                setCompletedCourseData(response1.data);
+                console.log(response1.data);
+            } catch (error) {
+                console.error('Error fetching course data:', error);
+                setError('Failed to fetch course data.');
+            }
+        };
+
+        fetchCourseData();
+    }, []);
+
+    return { enrolledCourseData, completedCourseData, error };
 }
