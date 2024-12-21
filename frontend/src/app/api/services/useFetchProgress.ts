7fdc7572a3fd0510b6b2441155af3c76c6dbede4
Ahmed Essam Ahmed Hassan <ahmed.essam@student.giu-uni.de>
2024-12-21 14:37:38 +0200

    dashboard upadte

--- frontend/src/app/api/services/useFetchProgress.ts
+++ frontend/src/app/api/services/useFetchProgress.ts
@@ -1,46 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 
 import { useState, useEffect } from 'react';
 import axios from '../../api/axios';
 
+
 export function useFetchUserProgresses(userId:string) {
     const [progressesData, setProgressData] = useState<any>([]);
-    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchProgressData = async () => {
             try {
                 const response = await axios.get(`/progress/user/${userId}`);
                 setProgressData(response.data);
-            } catch (error) {
-                console.error('Error fetching progresses data:', error);
-                setError('Failed to fetch progresses data.');
+            } catch {
+                console.error('Error fetching progresses data');
             }
         };
 
         fetchProgressData();
     }, [userId]);
 
-    return { progressesData, error };
+    return { progressesData };
 }
 
 export function useFetchCourseProgresses(courseId:string,userId:string) {
     const [progressData, setProgressData] = useState<any>([]);
-    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchProgressData = async () => {
             try {
                 const response = await axios.get(`progress/course/${courseId}/user/${userId}`);
                 setProgressData(response.data);
-            } catch (error) {
-                console.error('Error fetching progresses data:', error);
-                setError('Failed to fetch progresses data.');
+                console.log(response.data.completion_percentage);
+            } catch {
+                console.error('Error fetching progresses data');
             }
         };
 
         fetchProgressData();
-    }, [userId]);
+    }, [courseId ,userId]);
 
-    return { progressData, error };
+    return { progressData };
 }
--- frontend/src/components/StudentDashboardcourse.tsx
+++ frontend/src/components/StudentDashboardcourse.tsx
@@ -1,9 +1,10 @@
 "use client";
 import dashboardcss from '../styles/dashboard.module.css';
 import { useRouter } from 'next/navigation';
+import { useFetchCourseProgresses } from "../app/api/services/useFetchProgress";
+import { useAuth } from "@/components/AuthContext";
 
-
-export interface CourseDetailsProps {
+interface CourseDetailsProps {
     courseData: {
         _id: string;
         title: string;
@@ -13,19 +14,55 @@ export interface CourseDetailsProps {
     };
 }
 
+interface ProgressDetailsProps {
+    progressData: {
+        _id?: string;
+        user_id: { _id: string, name: string };
+        course_id: { _id: string, title: string };
+        completion_percentage: number;
+        last_accessed: Date;
+        completed_modules: { module_id: string; score: number }[];
+        average_quiz?: number;
+        opened_times: number;
+    };
+}
+
 export function AllCoursesDetails({ courseData }: CourseDetailsProps) {
+    const router = useRouter(); // Initialize the navigate function
+
+    const { tokenDetails } = useAuth();
+    const { progressData } = useFetchCourseProgresses(courseData._id, tokenDetails?._id);
+
+
+    const handleCourseClick = () => {
+        router.push(`/course/${courseData._id}`);
+    };
+
     return (
-        <div className={dashboardcss.courseTemplate}>
-            <h1>{courseData.title}</h1>
-            <p>Instructor: {courseData.instructor}</p>
-            <p>Description: {courseData.description}</p>
-            <p>Category: {courseData.category}</p>
+        <div className={dashboardcss.courseTemplate} onClick={handleCourseClick}>
+            <div className={dashboardcss.courseTemplateStates}>
+                <div className={dashboardcss.progressradial}></div>
+                <div className={dashboardcss.progresscircle} style={{ '--target-BG': `100% ${progressData.completion_percentage || 0}%` } as React.CSSProperties}></div>
+                <h1>Completion: {progressData.completion_percentage}%</h1>
+                <h1>Average Quiz Score: {progressData.average_quiz ?? 'N/A'}</h1>
+            </div>
+            <div className={dashboardcss.courseTemplatecontnet}>
+                <h1>{courseData.title}</h1>
+                <p>Description: {courseData.description}</p>
+                <p>Category: {courseData.category}</p>
+                <p>Instructor: {courseData.instructor}</p>
+                <p>Last Accessed: {new Date(progressData.last_accessed).toLocaleString()}</p>
+            </div>
         </div>
     );
 }
 
 export function EnrolledCourses({ courseData }: CourseDetailsProps) {
     const router = useRouter(); // Initialize the navigate function
+
+    const { tokenDetails } = useAuth();
+    const { progressData } = useFetchCourseProgresses(courseData._id, tokenDetails?._id);
+
 
     const handleCourseClick = () => {
         router.push(`/course/${courseData._id}`);
@@ -34,6 +71,9 @@ export function EnrolledCourses({ courseData }: CourseDetailsProps) {
     return (
         <li onClick={handleCourseClick}>
             {courseData.title}
+            <div className={dashboardcss.progress}>
+                <div className={dashboardcss.progressv} style={{ '--target-width': `${progressData.completion_percentage || 0}%` } as React.CSSProperties}></div>
+            </div>
         </li>
     );
 }
@@ -48,6 +88,9 @@ export function CompletedCourses({ courseData }: CourseDetailsProps) {
     return (
         <li onClick={handleCourseClick}>
             {courseData.title}
+            <div className={dashboardcss.progress}>
+                <div className={dashboardcss.progressv} style={{ '--target-width)': `100%` } as React.CSSProperties}></div>
+            </div>
         </li>
     );
 }
