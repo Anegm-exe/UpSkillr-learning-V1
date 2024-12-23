357627f094a2bc80eebdd110c50f8ffa728194c7
coolAbdo <abdelrahman.egschool@gmail.com>
2024-12-23 20:34:11 +0200

    forum works

--- frontend/src/app/forums/[forumid]/page.tsx
+++ frontend/src/app/forums/[forumid]/page.tsx
@@ -1,4 +1,25 @@
-export default function page() {
-    return <h1>Testers!</h1>;
-};
+'use client';
+import { useFetchForum } from "@/app/api/services/useFetchForum";
+import { useFetchCourseProgress } from "@/app/api/services/useFetchProgress";
+import { useAuth } from "@/components/AuthContext";
+import ForumDetails from "@/components/FroumDetails";
+import ProgressDetails from "@/components/ProgrerssDetails";
+import { useRouter } from "next/navigation";
+import React from "react";
 
+
+export default function ForumPage({ params } : { params:{forumid:string}}) {
+    const router = useRouter();
+    //@ts-ignore
+    const {forumid} = React.use(params);
+    const { tokenDetails } = useAuth();
+    //@ts-expect-error
+    const {forumData,refetch} = useFetchForum(forumid,tokenDetails?._id)
+
+      // Function to handle refreshing chat data after sending a message
+    const handleRefreshForum = () => {
+        refetch(); // Trigger the refetching of chat data to get updated messages
+    };
+
+    return <ForumDetails forumData={forumData} onBack={forumData} onNewMessageSent={handleRefreshForum} onDelete={() => router.push('/forums')}/>;
+}
