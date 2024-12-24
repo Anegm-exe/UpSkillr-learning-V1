7a68239e38f924f88b2a60dd243e3b51a7d87c45
Ahmed Essam Ahmed Hassan <ahmed.essam@student.giu-uni.de>
2024-12-24 03:04:24 +0200

    removed unnecessary frontend files!

--- frontend/src/app/course/[courseid]/solution/[solutionid]/page.tsx
+++
@@ -1,4 +0,0 @@
-export default async function CoursePage({ params } : { params:{solutionid:string} }) {
-    const solutionid = params.solutionid;
-    return <h1>Course Page no. {solutionid || 'i wanna die'}</h1>;
-}
